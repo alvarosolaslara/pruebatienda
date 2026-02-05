b49932b529c0c9315895699c7fa6b522dbbec4db
alvaro <asolas93@gmail.com>
2026-02-05 13:57:57 +0100

    Creación de  estructura estática, preparación para estilos, plantilla base

--- tienda_videojuegos/tienda_videojuegos/settings.py
+++ tienda_videojuegos/tienda_videojuegos/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'tienda_videojuegos.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],#Especificaremos el directorio de las platillas base, si no el extend no funcionará
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,3 +117,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'static'] #Base Dir representar el directorio base, principal de nuestro proyecto y static la carpeta stática, no confundir con core
+
