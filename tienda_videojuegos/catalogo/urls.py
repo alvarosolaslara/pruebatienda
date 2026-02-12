84cbb8027a4ee80cb408d3f0c4aae971cfa25af0
alvaro <asolas93@gmail.com>
2026-02-12 18:17:41 +0100

    Creación de la parte de detalle de cada producto y mejora de estilos de la web en general

--- tienda_videojuegos/catalogo/urls.py
+++ tienda_videojuegos/catalogo/urls.py
@@ -3,5 +3,6 @@ from.import views #importamos la vistas views.py
 #aunque no tengamos ninguna url definida será obligatorio tener urlpatterns, aunque sea bacía
 urlpatterns = [ 
     path('',views.lista_juegos, name='lista_juegos'),
+    path('<int:pk>/', views.detalle_juego, name='detalle_juego'),# Utilizaremos el primari key en modo int y se lo pasaremos a la url
     
 ]
--- tienda_videojuegos/catalogo/views.py
+++ tienda_videojuegos/catalogo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator
 from catalogo.models import Juego
 
@@ -19,5 +19,17 @@ def lista_juegos(request):
     #Pasamos la paginación a la página
     contexto_catalogo_juegos = {'lista_juegos':page_obj} #introduciremos el valor del dicionario en la variable
     return render(request, 'catalogo/lista_juegos.html', contexto_catalogo_juegos) # Pasaremos el valor del diccionario al html correspondiente
+
+
+
+def detalle_juego(request, pk):
+    # Obtenemos el juego concreto o mostramos un 404 si no existe
+    juego = get_object_or_404(Juego, pk=pk)
+
+    # Creamos el contexto que pasaremos a la plantilla
+    contexto = {'juego': juego}
+
+    # Renderizamos la plantilla de detalle
+    return render(request, 'catalogo/detalle_juego.html', contexto)
 
 
